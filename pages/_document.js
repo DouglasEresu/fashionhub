2b6e65cf6f2ff4d5cda2e7a5211dacd9890be4ee
DouglasEresu <eresudouglas9@gmail.com>
2023-04-17 17:57:18 +0100

    fix nav

--- pages/_document.js
+++ pages/_document.js
@@ -4,8 +4,8 @@ export default function Document() {
   return (
     <Html>
       <Head>
-      <meta name="viewport" content="width=device-width, initial-scale= -1" />
-
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossOrigin="anonymous"></link>
 
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
