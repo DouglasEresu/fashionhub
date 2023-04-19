451644bb4d897a72018471e1231c78cb757f589d
DouglasEresu <eresudouglas9@gmail.com>
2023-04-19 21:38:41 +0100

    nav

--- components/Nav.js
+++ components/Nav.js
@@ -9,9 +9,10 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-const Navigation = ()=> {
+const Navigation = (e)=> {
 
-  
+  e.preventDefault();
+
   const router =useRouter();
   const currentRoute = router.pathname;
 
