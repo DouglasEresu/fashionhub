96516d04e808d848557cdaef0ec04e061a2a982e
DouglasEresu <eresudouglas9@gmail.com>
2023-04-19 21:40:39 +0100

    nav

--- components/Nav.js
+++ components/Nav.js
@@ -9,7 +9,8 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-const Navigation = (e)=> {
+
+const Navigation = async(e)=> {
 
   e.preventDefault();
 
