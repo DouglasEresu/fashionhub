d1e143c6667045a33dede2ef4cd70f76520b5897
DouglasEresu <eresudouglas9@gmail.com>
2023-04-19 22:25:08 +0100

    nav

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import '../styles/globals.css'
 import Layout from '../components/Layout'
 import{Provider} from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react';
-
+import {persistor} from '../redux/store'
 import store from '../redux/store'
 
 function MyApp({ Component, pageProps }) {
